80b01618c400346402a5a2b548d346d8c21f0521
Rohit <rohittanger2@gmail.com>
2026-08-16 21:02:30 +0530

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String s = sc.nextLine();
+ 
+        HashSet<Character> set = new HashSet<>();
+ 
+        for (int i = 0; i < s.length(); i++) {
+            set.add(s.charAt(i));
+        }
+ 
+        if (set.size() % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        } else {
+            System.out.println("IGNORE HIM!");
+        }
+ 
+        sc.close();
+    }
+}
